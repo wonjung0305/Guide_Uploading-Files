82d2163604106050f4de827055ccec700a269842
wonjung <wonjung1919@gmail.com>
2026-07-26 17:17:21 +0900

    fix: FileuploadController에 StorageService import

--- src/main/java/com/example/gsuploadingfiles/FileUploadController.java
+++ src/main/java/com/example/gsuploadingfiles/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.example.gsuploadingfiles;
 
 import com.example.gsuploadingfiles.storage.StorageFileNotFoundException;
+import com.example.gsuploadingfiles.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
