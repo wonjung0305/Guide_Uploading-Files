839d9e36cd289b0577d4f0604f0f15191eba2cab
wonjung <wonjung1919@gmail.com>
2026-07-26 13:43:56 +0900

    chore: Project 생성

---
+++ src/main/java/com/example/gsuploadingfiles/GsUploadingFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsuploadingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsUploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsUploadingFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gsuploadingfiles/GsUploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsuploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsUploadingFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
